85d0c72997158731661cbb3b83009703b6226587
ting_li <ting.asgard@gmail.com>
2022-06-10 17:21:44 +0800

    feat:整併再保頁面

--- aandh/frontendah/src/main/resources/shigen/js/global.js
+++ aandh/frontendah/src/main/resources/shigen/js/global.js
@@ -9,6 +9,7 @@ $(function() {
 	$(".ajax-aside-navCMN").load("../../shigen/layout/ajax-aside-navCMN.html");
 	$(".ajax-section-main").load("../../shigen/layout/ajax-section-main.html");
 	$(".ajax-aside-navCLM").load("../../shigen/layout/ajax-aside-navCLM.html");
+	$(".ajax-aside-navRIN1").load("../../shigen/layout/ajax-aside-navRIN1.html");
 
 //	setTimeout(function() {
 //		$("a[href='login.html']").attr("href", "login");
